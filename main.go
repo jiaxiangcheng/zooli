6e5c2d29b54fd67111c04625cf92f58765dea364
qiaorui <qiaorui.xiang@gmail.com>
2018-02-20 16:04:28 +0100

    heroku fix

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"github.com/Qiaorui/zooli/models"
 	"github.com/astaxie/beego"
 	"github.com/asaskevich/govalidator"
+	"strconv"
+	"os"
 )
 
 func main() {
+
+	if port := os.Getenv("PORT"); port != "" {
+		beego.BConfig.Listen.HTTPPort, _ = strconv.Atoi(port)
+	}
 
 	govalidator.SetFieldsRequiredByDefault(true)
 	if err := models.Connect(); err != nil {
